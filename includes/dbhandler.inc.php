ebc23fac085bdaedf3dab42907bba42ba93c6d14
Jake Nguyen <jaketangnguyen@gmail.com>
2022-06-16 15:58:48 -0700

    fulltext search

--- includes/dbhandler.inc.php
+++ includes/dbhandler.inc.php
@@ -1,19 +1,19 @@
 <?php 
 
 // We need these 4 variables to connect to a database
-/*
+
 $dbServername = "us-faqtool-test:3307";
 $dbUsername = "webdev";
 $dbPassword = "SM95131*";
 $dbName = "FAQ";
-*/
 
-// Local
+
+/*// Local
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
 $dbName = "FAQ";
-
+*/
 $conn = mysqli_connect(
     $dbServername, 
     $dbUsername, 
